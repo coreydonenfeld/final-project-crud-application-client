df6e0d21a36bb2af2c83e086ed8202f1a56dea27
Corey Donenfeld <corey@studiosimpati.co>
2023-05-15 09:51:37 -0400

    Campus View + Campus List + Styling

--- src/components/modules/CampusList.js
+++ src/components/modules/CampusList.js
@@ -9,22 +9,26 @@ const CampusList = ({ campuses }) => {
         );
     }
     return (
-        <ul className="grid">
+        <ul className="grid campus-list">
             {campuses.map((campus) => {
                 let Image = null;
                 if (campus.imageUrl !== null) {
-                    Image = <img src={campus.imageUrl} alt={campus.name} />;
+                    Image = (
+                        <Link to={`/campus/${campus.id}`} className="image-wrapper">
+                            <img src={campus.imageUrl} alt={campus.name} />
+                        </Link>
+                    );
                 }
                 return (
-                    <li key={campus.id}>
+                    <li key={campus.id} className="campus-item">
                         {Image}
                         <Link to={`/campus/${campus.id}`}>
                             <h3 className="heading-5">{campus.name}</h3>
                         </Link>
-                        <p>{campus.address}</p>
-                        <p>{campus.description}</p>
-                        <Button link={`/campus/${campus.id}`} type="secondary">View Campus</Button>
+                        <p className="address">{campus.address}</p>
+                        <p className="description">{campus.description}</p>
 
+                        <p>{campus.students.length} students</p>
                         {
                             campus.students.length > 0 &&
                             <div>
@@ -40,6 +44,8 @@ const CampusList = ({ campuses }) => {
                                 </ul>
                             </div>
                         }
+
+                        <Button link={`/campus/${campus.id}`} type="secondary">View Campus</Button>
                     </li>
                 );
             })}
