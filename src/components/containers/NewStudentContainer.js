f675c4a9e5443b0c49229d7958620076491c87b1
Corey Donenfeld <corey@studiosimpati.co>
2023-05-14 22:00:49 -0400

    Tiny tweak

--- src/components/containers/NewStudentContainer.js
+++ src/components/containers/NewStudentContainer.js
@@ -68,7 +68,7 @@ class NewStudentContainer extends Component {
     render() {
         // Redirect to new student's page after submit
         if (this.state.redirect) {
-            return (<Redirect to={`/student/${this.state.redirectId}`} />)
+            return (<Redirect to={`/student/${this.state.redirectId}`} />);
         }
 
         // Display the input form via the corresponding View component
